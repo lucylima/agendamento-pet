f1df13ab0b961b826bcb41b01c0e570817c88819
Lucy Lima <lucyatmosphere@gmail.com>
2025-03-23 17:23:37 -0300

    feat: added icons, styling, folder organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
+import sunIcon from './assets/svg/sun-icon.svg'
+import logo from './assets/svg/mundo-pet-logo.svg'
+import styles from './assets/scss/App.module.scss'
+
 function App() {
   return (
     <>
-      <div>
-        <img />
+      <div className="logo">
+        <img src={logo} className="logo__image" />
       </div>
       <main>
-        <div className="header">
+        <div className={styles.header}>
           <div>
             <h2> Sua agenda </h2>
             <p>
-              Aqui você pode ver todos os clientes e serviços agendados para
-              hoje.
+              Aqui você pode ver todos os clientes e serviços
+              agendados para hoje.
             </p>
           </div>
           <input type="date" />
         </div>
         <div className="list">
           <div className="morning">
-            <div className="monring--header">
-              <div> icon Manhã</div>
-              <span> 09h-12h </span>
+            <div className="monring__header">
+              <img src={sunIcon} />
+              <span className=""> 09h-12h </span>
             </div>
-            <ul>
-              <li>
-                <span> 09:00 </span>{" "}
-                <div>
-                  {" -"}
-                  <span> Thor </span> <span>/ Fernanda Costa </span>{" "}
+            <ul className="clients__container">
+              <li className="clients__item">
+                <span className="item__schedule"> 09:00 </span>
+                <div className="item__info">
+                  <span className="pet-name"> Thor </span>{' '}
+                  <span className="owner-name">
+                    / Fernanda Costa{' '}
+                  </span>
                 </div>
-                <span> Vacinação </span>
-                <button> Remover Agendamento </button>
+                <span className="item__service"> Vacinação </span>
+                <button className="item__button">
+                  Remover Agendamento
+                </button>
               </li>
             </ul>
           </div>
         </div>
       </main>
+      <button>Novo agendamento</button>
     </>
-  );
+  )
 }
 
-export { App };
+export { App }
